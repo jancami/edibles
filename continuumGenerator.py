8d92fcf515451dd71caab68e9cc7bc37c854f35f
Klay Kulik <klay@bonamassa.astro.uwo.ca>
2019-02-12 18:29:59 -0500

    made R 1 m/s

--- continuumGenerator.py
+++ continuumGenerator.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy.interpolate import CubicSpline
-from scipy.stats import chisquare
 import matplotlib.pyplot as plt
+from astropy.modeling.models import Voigt1D
+from astropy import constants as cst
+
 
 import sys
 import os
@@ -31,6 +33,25 @@ def generateContinuum(x, y, n_sections=4, move_x=False, move_y=True):
 	y_cont: [ndarray] flux value points for fit continuum
 	'''
 
+	# # resolving power of 0.1 km/s
+	# R = cst.c.to('cm/s').value * 1.0e-5 / 0.1
+
+	# make resolving power 1 m/s
+	R = cst.c.to('m/s').value
+
+	# print('Resolotion: ', R)
+
+
+	# delta_lambda = lambda_peak / R
+	# xmin = min(x)
+	# xmax = max(x)
+	# x_nonbroad = np.arange(xmin, xmax, delta_lambda)
+	# # using this method for making grid increase fitting time so much
+	# # x_nonbroad = mg.make_grid(xmin, xmax, resolution=R)
+
+	x_nonbroad = make_grid(np.min(x), np.max(x), resolution=R)
+	x_spline = np.array(x_nonbroad)
+
 	x_sections = np.array_split(x, n_sections)
 	y_sections = np.array_split(y, n_sections)
 
@@ -42,16 +63,12 @@ def generateContinuum(x, y, n_sections=4, move_x=False, move_y=True):
 		x_value_array.append(x_value)
 		y_value_array.append(y_value)
 
-
 	spline = CubicSpline(x_value_array, y_value_array)
-
-
-	x_spline = np.linspace(np.min(x), np.max(x), (len(x)))
+	# x_spline = np.linspace(np.min(x), np.max(x), (len(x)))
 	y_spline = spline(x_spline)
 
 	# # plot median points of each section
 	# plt.plot(x_value_array, y_value_array, 'ko')
-
 
 	return x_spline, y_spline
 
@@ -60,16 +77,16 @@ def generateContinuum(x, y, n_sections=4, move_x=False, move_y=True):
 
 alpha, gamma = 0.1, 0.1
 
-wave = np.linspace(-3, 3, 1000)
-flux = -voigt_math(wave, alpha, gamma)
+wave = np.linspace(7663, 7670, 1000)
+# flux = -voigt_math(wave, alpha, gamma)
+voigt_func = Voigt1D(x_0=7667, amplitude_L=10, fwhm_L=0.1, fwhm_G=0.2)
+flux = -1 * voigt_func(wave)
 
 # Generate the spline curve
 x_spline, y_spline = generateContinuum(wave, flux)
 
+
 plt.plot(wave, flux, marker='o', markersize='3', label='Data')
 plt.plot(x_spline, y_spline, label='Spline fit')
 plt.legend()
-plt.show()
-
-
 plt.show()
