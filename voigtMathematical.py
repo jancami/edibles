d01fa4ce68b75c732961d34c27467372501f881e
Klay Kulik <klay@bonamassa.astro.uwo.ca>
2019-02-13 16:33:39 -0500

    added central wavelength term

--- voigtMathematical.py
+++ voigtMathematical.py
@@ -3,13 +3,14 @@ from scipy.special import wofz
 # import matplotlib.pyplot as plt
 
 
-def voigt_math(x, alpha, gamma, x_pts=None, y_pts=None, cont=False):
+def voigt_math(x, cent, alpha, gamma, x_pts=None, y_pts=None, cont=False):
     """
     Return the Voigt line shape at x with Lorentzian component HWHM gamma
     and Gaussian component HWHM alpha.
 
     Input:
     x:      [ndarray]  Data grid
+    cent:   [float]    Peak of the Voigt profile
     alpha:  [float]    Gaussian HWHM component
     gamma:  [float]    Lorentzian HWHM component
     x_pts:  [ndarray]  x-components of initial continuum guess
@@ -24,12 +25,12 @@ def voigt_math(x, alpha, gamma, x_pts=None, y_pts=None, cont=False):
         # call continuum function here?
         print("Do something")
 
-    return np.real(wofz((x + 1j*gamma)/sigma/np.sqrt(2))) / sigma/np.sqrt(2*np.pi)
+    return np.real(wofz(((x - cent) + 1j*gamma)/sigma/np.sqrt(2))) / sigma/np.sqrt(2*np.pi)
 
 
 # alpha, gamma = 0.1, 0.1
-# x = np.linspace(-0.8,0.8,1000)
-# y = voigt_math(x, alpha, gamma)
+# x = np.linspace(0,3,1000)
+# y = voigt_math(x, 2, alpha, gamma)
 
 # plt.plot(x, y, label='Voigt')
 # # plt.xlim(-0.8,0.8)
