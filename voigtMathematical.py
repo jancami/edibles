c6436c9c2391947e7ac77d02b0765cfbc94549f2
Klay Kulik <kulik@localhost.localdomain>
2019-02-21 14:43:22 -0500

    cleaned function, added changavle delta_v parameter

--- voigtMathematical.py
+++ voigtMathematical.py
@@ -1,11 +1,10 @@
 import numpy as np
 from scipy.special import wofz
-# import matplotlib.pyplot as plt
 
 
-def voigt_math(x, cent, alpha, gamma, x_pts=None, y_pts=None, cont=False):
+def voigt_math(x, cent, alpha, gamma, delta_v=1.0):
     """
-    Return the Voigt line shape at x with Lorentzian component HWHM gamma
+    Return the Voigt line shape centered at cent with Lorentzian component HWHM gamma
     and Gaussian component HWHM alpha.
 
     Input:
@@ -13,26 +12,9 @@ def voigt_math(x, cent, alpha, gamma, x_pts=None, y_pts=None, cont=False):
     cent:   [float]    Peak of the Voigt profile
     alpha:  [float]    Gaussian HWHM component
     gamma:  [float]    Lorentzian HWHM component
-    x_pts:  [ndarray]  x-components of initial continuum guess
-    y_pts:  [ndarray]  y-components of initial continuum guess
-    cont:   [bool]     Use a continuum term? Default is False
-
+    delta_v: [float] desired resolution of data (in m/s)
     """
+
     sigma = alpha / np.sqrt(2 * np.log(2))
 
-
-    if cont is True:
-        # call continuum function here?
-        print("Do something")
-
     return np.real(wofz(((x - cent) + 1j*gamma)/sigma/np.sqrt(2))) / sigma/np.sqrt(2*np.pi)
-
-
-# alpha, gamma = 0.1, 0.1
-# x = np.linspace(0,3,1000)
-# y = voigt_math(x, 2, alpha, gamma)
-
-# plt.plot(x, y, label='Voigt')
-# # plt.xlim(-0.8,0.8)
-# plt.legend()
-# plt.show()
