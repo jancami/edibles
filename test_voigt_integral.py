2d3554df2c5749c3a5fa06a4f5dfbb37ee712f05
Klay Kulik <klay@bonamassa.astro.uwo.ca>
2019-02-25 15:14:44 -0500

    made normalize() a function that is callable in other scripts

--- test_voigt_integral.py
+++ test_voigt_integral.py
@@ -1,59 +1,41 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from astropy import constants as cst
-
 import sys
-import os
-path = os.getcwd()
-os.chdir('..')
-sys.path.append(os.getcwd())
-os.chdir(path)
-
-from edibles.voigtMathematical import voigt_math
-from edibles.fit.make_grid import make_grid
 
 
-# set params
-alpha = 0.1
-gamma = 0.1
-delta_v = 10
-x_min = 7665
-x_max = 7669
-cent = 7667
-n_piece = 3
+def normalize(x, y):
+	'''
+	This function checks if the area under the curve produced using the x
+	and y arrays is equal to one. If it is not equal to one, the y axis is
+	recursively normalized until its integral is 1.0.
 
+	INPUT:
+	x: [ndarray] input x axis
+	y: [ndarray] input y axis
+	
+	OUTPUT:
+	y_norm: [ndarray] output normalized y axis
 
-# generate wavelength grid with resolving power delta_v (R = c/delta_v)
-R = cst.c.value / delta_v
-x_nonbroad = make_grid(x_min, x_max, resolution=R)
-wave = np.array(x_nonbroad)
+	'''
 
+	# Check to see if area under curve equals 1.0
+	area = np.trapz(y, x)
 
-# generate voigt data with specified parameters
-flux = voigt_math(wave, cent, alpha, gamma)
+	print('Initial area under curve: {:.16f}'.format(area))
 
-# compute area under curve
-area = np.trapz(flux, wave)
-print('Area under curve: ', area)
+	# loop until normalized (maybe)
+	i = 0
+	while area != 1.0:
+		i = i + 1
+		print('Normalizing attempt number: {}'.format(i))
+		y = y / area
+		area = np.trapz(y, x)
 
+		print('Area under curve: {:.16f}'.format(area))
+		print(area.is_integer())
+		if i >1000:
+			print('Area under curve could not be normalized to one!')
+			sys.exit()
 
-# loop until normalized (maybe)
-i = 0
-while area != 1.0:
-	print('Normalizing')
-	flux = flux / area
-	area = np.trapz(flux, wave)
+		y_norm = y
 
-	print('Area under curve: ', area)
-	print(area.is_integer())
-	i = i + 1
-	if i >1000:
-		print('Area under curve could not be normalized to one!')
-		continue
-
-
-# plot
-plt.plot(wave, flux, 'k.', markersize='1')
-
-plt.legend()
-plt.show()
+	return y_norm
