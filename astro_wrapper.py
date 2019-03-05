069644fa58395f12878c040c8b81052f00138403
Klay Kulik <klay@bonamassa.astro.uwo.ca>
2019-03-05 13:10:10 -0500

    working on astro function

--- astro_wrapper.py
+++ astro_wrapper.py
@@ -1,5 +1,6 @@
 import numpy as np
 from astropy import constants as cst
+from scipy.special import wofz
 
 
 def voigtMathToAstro(nu, nu0, alpha, gamma):
@@ -17,7 +18,17 @@ def voigtMathToAstro(nu, nu0, alpha, gamma):
 
 	'''
 
+	sigma = alpha / np.sqrt(2 * np.log(2))
+	delta_nu = nu - nu0
+	delta_nu_D = sigma * np.sqrt(2)
+	Gamma = 4 * np.pi * gamma
+	b = cst.c.value / nu * delta_nu_D
 
+	vgt = 1. / (delta_nu_D * np.sqrt(np.pi)) \
+		* wofz((delta_nu / delta_nu_D) + 1j * (Gamma / (4 * np.pi * delta_nu_D)))
+
+	tau = (10**log_N) * sigma0 * osc_freq * vgt
+	voigt_model = np.exp(-tau) - 1
 
 	# # Amin's
 	# nu = cst.c.to('cm/s').value / (x * 1.e-8)
@@ -31,26 +42,22 @@ def voigtMathToAstro(nu, nu0, alpha, gamma):
 	# tau = (10**log_N) * sigma0 * osc_freq * vgt
 	# voigt_model = np.exp(-tau) - 1
 
+	# # From Jan's book
+	# N = '?'	  	# column density
+	# g1 = '?'  	# statistical weight of level 1
+	# B12 = '?'  	# Einstein absorption coefficient
+	# E1 = '?'  	# Energy of level 1
+	# T = '?'  	# Temperature
+
+	# def P(T):
+	# 	'''
+	# 	Partition function at temp T
+	# 	'''
+	# 	return '?'
+
+	# tau = N * (cst.h.value * nu0 / (4*np.pi)) 							\
+	# 	* g1 * B12 * (np.exp(-E1 / (cst.k.value * T)) / P(T)) 			\
+	# 	* (1 - np.exp(-cst.h.value * nu0 / (cst.k.value * T))) * nu
 
 
-	N = '?'	  	# column density
-	g1 = '?'  	# statistical weight of level 1
-	B12 = '?'  	# Einstein absorption coefficient
-	E1 = '?'  	# Energy of level 1
-	T = '?'  	# Temperature
-
-	def P(T):
-
-		'''
-		Partition function at temp T
-
-		'''
-
-		return '?'
-
-	tau = N * (cst.h.value * nu0 / (4*np.pi)) 							\
-		* g1 * B12 * (np.exp(-E1 / (cst.k.value * T)) / P(T)) 			\
-		* (1 - np.exp(-cst.h.value * nu0 / (cst.k.value * T))) * nu
-
-
-	return tau
+	return voigt_model
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ os.chdir(path)
 
 from edibles.continuum_guess import generate_continuum
 from edibles.voigtMathematical import voigt_math
+from edibles.frequency_convert import convert_to_nu
 from edibles.fit.make_grid import make_grid
 # from edibles.test_voigt_integral import normalize
 
@@ -35,21 +36,32 @@ x_nonbroad = make_grid(x_min, x_max, resolution=R)
 wave = np.array(x_nonbroad)
 
 
+
+
 # generate voigt data with specified parameters
 flux_norm = voigt_math(wave, cent, alpha, gamma)
 
+nu, nu0 = convert_to_nu(wave, cent)
+print(nu0)
 
 # normalize the flux values
 # flux_norm = normalize(wave, flux, show=True)
 
 # flux_norm = -flux_norm + 1
 
+# check x arrays
+if np.array_equal(nu, wave) is not True:
+	print(len(wave))
+	print(len(nu))
+	print('Bad x resolution!')
+
+
 # Generate the continuum data
-x_spline, y_spline = generate_continuum(wave, flux_norm, delta_v, n_piece)
+x_spline, y_spline = generate_continuum(nu, flux_norm, delta_v, n_piece)
 
 
 # check x arrays
-if np.array_equal(wave, x_spline) is not True:
+if np.array_equal(nu, x_spline) is not True:
 	print('Bad x resolution!')
 
 
@@ -68,10 +80,11 @@ cxv = y_spline * flux_norm
 
 
 # plot
-plt.plot(wave, flux_norm, 'k.', markersize='1', label='Data')
+plt.plot(nu, flux_norm, 'k.', markersize='1', label='Data')
+plt.xlabel('Frequency')
 plt.plot(x_spline, y_spline, label='Spline fit')
-plt.plot(wave, cxv, label='C*V')
-# plt.plot(wave, resid, label='Residuals')
+plt.plot(nu, cxv, label='C*V')
+# plt.plot(nu, resid, label='Residuals')
 
 plt.legend()
 plt.show()
