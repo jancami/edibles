102cf4abaa6e8d70e4477334d760a27ee07e2dd5
Klay Kulik <kulik@dyn172-30-71-190.wireless.uwo.pri>
2019-03-05 15:38:34 -0500

    units, added alpha/gamma to converter

--- frequency_convert.py
+++ frequency_convert.py
@@ -1,15 +1,16 @@
 from astropy import constants as cst
 
-def convert_to_nu(x, cent):
+def convert_to_nu(x, cent, alpha, gamma):
 	'''
 	Short function that converts wavelength to frequency
 
-	INPUT:
+	INPUT:  [Angstroms]
 
 	x:      [ndarray]  Wavelength data grid
     cent:   [float]    Wavelength peak of the Voigt profile
 	
-	OUTPUT:
+
+	OUTPUT: [Hz]
 
 	nu:		[ndarray]  Frequency data grid
 	nu0:	[float]    Frequency peak of the Voigt profile
@@ -17,5 +18,7 @@ def convert_to_nu(x, cent):
 
 	nu = cst.c.value / (x * 1.e-10)
 	nu0 = cst.c.value / (cent * 1.e-10)
+	a1 = alpha*nu0**2/cst.c.value*1.e-10
+	g1 = gamma*nu0**2/cst.c.value*1.e-10
 
-	return nu, nu0
+	return nu, nu0, a1, g1
--- voigtMathematical.py
+++ voigtMathematical.py
@@ -2,18 +2,19 @@ import numpy as np
 from scipy.special import wofz
 
 
-def voigt_math(nu, nu0, alpha, gamma):
+def voigt_math(nu, nu0, a1, g1):
     """
-    Return the Voigt line shape centered at nu0 with Lorentzian component HWHM gamma
-    and Gaussian component HWHM alpha.
+    Return the Voigt line shape centered at nu0 with Lorentzian component HWHM g1
+    and Gaussian component HWHM a1.
 
-    Input:
+    Input:  [Hz]
+
     nu:     [ndarray]  Data grid
     nu0:    [float]    Peak of the Voigt profile
-    alpha:  [float]    Gaussian HWHM component
-    gamma:  [float]    Lorentzian HWHM component
+    a1:     [float]    Gaussian HWHM component
+    g1:     [float]    Lorentzian HWHM component
     """
 
-    sigma = alpha / np.sqrt(2 * np.log(2))
+    sigma = a1 / np.sqrt(2 * np.log(2))
 
-    return np.real(wofz(((nu - nu0) + 1j*gamma)/sigma/np.sqrt(2))) / sigma/np.sqrt(2*np.pi)
+    return np.real(wofz(((nu - nu0) + 1j*g1)/sigma/np.sqrt(2))) / sigma/np.sqrt(2*np.pi)
