b325e23e4bc1c8ef8978627128f1af3b76a4e05a
Klay Kulik <kulik@dyn172-30-71-190.wireless.uwo.pri>
2019-03-05 18:21:19 -0500

    alpha and gamma are optional, choose which direction to convert

--- frequency_convert.py
+++ frequency_convert.py
@@ -1,6 +1,6 @@
 from astropy import constants as cst
 
-def convert_to_nu(x, cent, alpha, gamma):
+def converter(x, cent, alpha=None, gamma=None, units=None):
 	'''
 	Short function that converts wavelength to frequency
 
@@ -8,6 +8,7 @@ def convert_to_nu(x, cent, alpha, gamma):
 
 	x:      [ndarray]  Wavelength data grid
     cent:   [float]    Wavelength peak of the Voigt profile
+    type:   [str] 	   Angstroms or Hz
 	
 
 	OUTPUT: [Hz]
@@ -16,9 +17,52 @@ def convert_to_nu(x, cent, alpha, gamma):
 	nu0:	[float]    Frequency peak of the Voigt profile
 	'''
 
+	a1 = None
+	g1 = None
+
+
 	nu = cst.c.value / (x * 1.e-10)
 	nu0 = cst.c.value / (cent * 1.e-10)
-	a1 = alpha*nu0**2/cst.c.value*1.e-10
-	g1 = gamma*nu0**2/cst.c.value*1.e-10
+
+	if alpha is not None:
+		if units == 'Angstroms':
+			a1 = alpha*nu0**2/cst.c.value*1.e-10
+		if units == 'Hz':
+			a1 = alpha*cst.c.value/(nu0**2*1.e-10)
+
+
+	if gamma is not None:
+
+		if units == 'Angstroms':
+			g1 = gamma*nu0**2/cst.c.value*1.e-10
+
+		if units == 'Hz':
+			g1 = gamma*cst.c.value/(nu0**2*1.e-10)
+
+	
+	if a1 is None:
+		if g1 is None:
+			return nu, nu0
+
+		# a1 is None, g1 is not None
+		return nu, nu0, g1
+
+	# beyond here, a1 is not None
+	if g1 is None:
+		return nu, nu0, a1
+
 
 	return nu, nu0, a1, g1
+
+
+
+
+
+	# try:
+	# 	return nu, nu0, a1, g1
+	# except UnboundLocalError:
+	# 	return nu, nu0, a1
+	# except UnboundLocalError:
+	# 	return nu, nu0, g1
+	# except UnboundLocalError:
+	# 	return nu, nu0
