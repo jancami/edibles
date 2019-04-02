4cd837b327287cceea0db028edc970fbb084db98
Klay Kulik <klay@bonamassa.astro.uwo.ca>
2019-04-02 14:36:55 -0400

    Made it possible to import this function without running the example

--- functions/print_header.py
+++ functions/print_header.py
@@ -10,5 +10,5 @@ def print_header(input_fits):
     hdu = fits.open(input_fits)
     print(hdu[0].header)
 
-
-print_header('/data/FITS/HD170740/RED_860/HD170740_w860_n12_20160613_L.fits')
+if __name__ == '__main__':
+	print_header('/data/FITS/HD170740/RED_860/HD170740_w860_n12_20160613_L.fits')
