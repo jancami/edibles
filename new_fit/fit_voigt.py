f3bc3c590af9ed4e569e3871bcc84cb302ea0dd7
Ranjan <ranjany74@gmail.com>
2019-05-26 16:07:11 -0400

    chk

--- new_fit/fit_voigt.py
+++ new_fit/fit_voigt.py
@@ -134,7 +134,7 @@ def fitter(file, xmin, xmax, peak_cutoff=0.5, n_points=5, alpha=0.05, gamma=0.00
         # create temporary voigt object
         obj = Voigt1D()
         obj.cent          = wave_subset[peaks[i]]  #  7664.87
-        # obj.cent.frozen = False
+        obj.cent.frozen = False
         obj.alpha         = alpha
         obj.gamma         = gamma
         obj.scaling       = scaling
@@ -199,9 +199,9 @@ if __name__ == "__main__":
     # load data from file
 
     # # NaI 5890/5896
-    # hdu = fits.open('/data/DR3_fits/HD170740/RED_564/HD170740_w564_n9_20160612_U.fits')
-    # x_min = 5885.
-    # x_max = 5898.
+    file = '/home/ranjan/python/data/DR3_fits/HD170740/RED_564/HD170740_w564_n9_20160612_U.fits'
+    xmin = 5885.
+    xmax = 5898.
 
     # # NaI 3000
     # hdu = fits.open('/data/DR3_fits/HD170740/BLUE_346/HD170740_w346_n6_20160612_B.fits')
@@ -209,15 +209,15 @@ if __name__ == "__main__":
     # x_max = 3305.
 
     # KI 7665
-    file = '/data/DR3_fits/HD170740/RED_860/HD170740_w860_redl_20140915_O12.fits'
+    # file = '/data/DR3_fits/HD170740/RED_860/HD170740_w860_redl_20140915_O12.fits'
 
-    xmin = 7662
-    xmax = 7670
+    # xmin = 7662
+    # xmax = 7670
     peak_cutoff = 0.5
     n_points = 5
     alpha = 0.05
     gamma = 0.005
-    scaling = 1.
+    scaling = 75.
 
 
     fitter(file, xmin=xmin, xmax=xmax, peak_cutoff=peak_cutoff, n_points=n_points, 
