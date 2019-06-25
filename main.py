acca0b90dd8d5ea79dc088c9cc3b37b4f42cea4b
Klay Kulik <klaykulik@gmail.com>
2019-06-25 16:25:31 -0400

    working on main.py

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ from sherpa.optmethods import LevMar, MonCar, NelderMead
 from sherpa.fit import Fit
 from sherpa.plot import DataPlot, ModelPlot, FitPlot
 
+import time
 
+start = time.time()
 
 # ===========
 # Main script
@@ -99,7 +101,7 @@ if n_points >= 7:
 if n_points >= 8:
     cont.y8            = y_points1[7]
     cont.y8.frozen     = False
-print(cont)
+# print(cont)
 
 
     # add cont to model
@@ -113,34 +115,33 @@ model = cont
 # find peaks
 prominence = (np.max(flux) - np.min(flux)) * peak_cutoff
 peaks, _ = find_peaks(-flux, prominence=prominence)
-print(peaks)
+# print(peaks)
 
-plt.plot(wave, flux)
-plt.plot(wave[peaks], flux[peaks], 'x')
+# plt.plot(wave, flux)
+# plt.plot(wave[peaks], flux[peaks], 'x')
 
-plt.show()
-# frozen = false for all
+# plt.show()
 
 line1 = VoigtAbsorptionLine()
 line1.lam_0          = wave[peaks[2]]
 line1.b              = b_1
 line1.d              = d_1
 line1.tau_0          = tau_0_2
-print(line1)
+# print(line1)
 
 line2 = VoigtAbsorptionLine()
 line2.lam_0          = wave[peaks[3]]
 line2.b              = b_2
 line2.d              = d_2
 line2.tau_0          = tau_0_3
-print(line2)
+# print(line2)
 
 line3 = VoigtAbsorptionLine()
 line3.lam_0          = wave[peaks[4]]
 line3.b              = b_2
 line3.d              = d_2
 line3.tau_0          = tau_0_4
-print(line3)
+# print(line3)
 
 
 
@@ -149,35 +150,35 @@ line4.lam_0          = wave[peaks[0]]
 line4.b              = 1.5
 line4.d              = d_2
 line4.tau_0          = tau_0_0
-print(line4)
+# print(line4)
 line5 = VoigtAbsorptionLine()
 line5.lam_0          = wave[peaks[1]]
 line5.b              = 1.5
 line5.d              = d_2
 line5.tau_0          = tau_0_1
-print(line5)
+# print(line5)
 line6 = VoigtAbsorptionLine()
 line6.lam_0          = wave[peaks[5]]
 line6.b              = 1.5
 line6.d              = d_2
 line6.tau_0          = tau_0_5
-print(line6)
+# print(line6)
 line7 = VoigtAbsorptionLine()
 line7.lam_0          = wave[peaks[6]]
 line7.b              = 1.5
 line7.d              = d_2
 line7.tau_0          = tau_0_6
-print(line7)
+# print(line7)
 
 
 # multiply lines by model
 model *= line1
 model *= line2
 model *= line3
-model *= line4
-model *= line5
-model *= line6
-model *= line7
+# model *= line4
+# model *= line5
+# model *= line6
+# model *= line7
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -193,13 +194,13 @@ d = Data1D('2014-09-15', wave, flux)
     # Dataset 1
 dplot = DataPlot()
 dplot.prepare(d)
-dplot.plot()
+# dplot.plot()
 
 mplot = ModelPlot()
 mplot.prepare(d, model)
-dplot.plot()
-mplot.overplot()
-plt.show()
+# dplot.plot()
+# mplot.overplot()
+# plt.show()
 
 
 # =========================================
@@ -243,4 +244,10 @@ plt.plot(wave, flux-model(wave))
 plt.xlabel('Wavelength (AA)', fontsize=12)
 plt.ylabel('Flux', fontsize=12)
 plt.tick_params(axis='both', labelsize=12)
+
+
+duration = time.time() - start
+print('Time taken: ' + str(duration))
+
+
 plt.show()
