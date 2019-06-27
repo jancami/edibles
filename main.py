25322d705a765f0a49bcbd43ce843ad9aacdfa2d
Klay Kulik <klaykulik@gmail.com>
2019-06-27 14:23:13 -0400

    catching up with master

--- main.py
+++ main.py
@@ -17,47 +17,7 @@ xmax = 3305.
 data = load_fits_range(file, xmin, xmax)
 wave, flux = data
 
-    # Auto-generate continuum guess parameters - no need to do it manually
 
-
-
-y_spline1, y_points1= generate_continuum((wave, flux), delta_v=1000, n_piece=n_piece)
-
-
-
-# create cont object and define parameters
-# ----------------------------------------
-
-cont = Cont1D()
-
-    # always at least 2 points / 1 piece
-if n_points >= 1:
-    cont.y1            = y_points1[0]
-    cont.y1.frozen     = False
-if n_points >= 2:
-    cont.y2            = y_points1[1]
-    cont.y2.frozen     = False
-if n_points >= 3:
-    cont.y3            = y_points1[2]
-    cont.y3.frozen     = False
-if n_points >= 4:
-    cont.y4            = y_points1[3]
-    cont.y4.frozen     = False
-if n_points >= 5:
-    cont.y5            = y_points1[4]
-    cont.y5.frozen     = False
-if n_points >= 6:
-    cont.y6            = y_points1[5]
-    cont.y6.frozen     = False
-if n_points >= 7:
-    cont.y7            = y_points1[6]
-    cont.y7.frozen     = False
-if n_points >= 8:
-    cont.y8            = y_points1[7]
-    cont.y8.frozen     = False
-# print(cont)
-
-=======
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Continuum
 n_points = 4
