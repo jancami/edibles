a5b33c1b218694802419e523c6ca3435452e49bf
Klay Kulik <klaykulik@gmail.com>
2019-06-27 14:29:57 -0400

    commented in different star

--- main.py
+++ main.py
@@ -8,56 +8,24 @@ from edibles.functions.load_fits_range import load_fits_range
 
 
 # file params
+
 star_name = 'HD170740'
-file = '/data/DR3_fits/HD147084/BLUE_346/HD147084_w346_n10_20160419_B.fits'
-xmin = 3300.
-xmax = 3305.
+file = '/data/DR3_fits/HD170740/RED_860/HD170740_w860_n20_20140916_L.fits'
+xmin = 7661.
+xmax = 7670.
+
+# star_name = 'HD170740'
+# file = '/data/DR3_fits/HD147084/BLUE_346/HD147084_w346_n10_20160419_B.fits'
+# xmin = 3300.
+# xmax = 3305.
+
+
 
 
 data = load_fits_range(file, xmin, xmax)
 wave, flux = data
 
-    # Auto-generate continuum guess parameters - no need to do it manually
 
-
-
-y_spline1, y_points1= generate_continuum((wave, flux), delta_v=1000, n_piece=n_piece)
-
-
-
-# create cont object and define parameters
-# ----------------------------------------
-
-cont = Cont1D()
-
-    # always at least 2 points / 1 piece
-if n_points >= 1:
-    cont.y1            = y_points1[0]
-    cont.y1.frozen     = False
-if n_points >= 2:
-    cont.y2            = y_points1[1]
-    cont.y2.frozen     = False
-if n_points >= 3:
-    cont.y3            = y_points1[2]
-    cont.y3.frozen     = False
-if n_points >= 4:
-    cont.y4            = y_points1[3]
-    cont.y4.frozen     = False
-if n_points >= 5:
-    cont.y5            = y_points1[4]
-    cont.y5.frozen     = False
-if n_points >= 6:
-    cont.y6            = y_points1[5]
-    cont.y6.frozen     = False
-if n_points >= 7:
-    cont.y7            = y_points1[6]
-    cont.y7.frozen     = False
-if n_points >= 8:
-    cont.y8            = y_points1[7]
-    cont.y8.frozen     = False
-# print(cont)
-
-=======
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Continuum
 n_points = 4
