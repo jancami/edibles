ec96df0efd844b461479dd49c595a274ace2e721
Jan Cami <jancami@iMacNaj-3.local>
2019-07-03 10:56:00 -0400

    First draft of edibles_spectrum

---
+++ edibles_spectrum.py
@@ -0,0 +1,39 @@
+import numpy as np
+from astropy.io import fits
+import matplotlib.pyplot as plt
+
+class edibles_spectrum:
+# This object will contain a spectrum from EDIBLES, and a set of methods to operate on the data. 
+
+	def load_spectrum (self):
+		# Assume the file is a DR3 product here. 
+		hdu = fits.open(self.filename)
+		self.header = hdu[0].header
+		self.flux = hdu[0].data
+		self.flux_units="arbitrary"
+		crval1 = hdu[0].header["CRVAL1"]
+		cdelt1 = hdu[0].header["CDELT1"]
+		nwave = len(self.flux)
+		grid = np.arange(0, nwave, 1)
+		self.wave = (grid) * cdelt1 + crval1
+		self.wave_units = "AA"
+		self.reference_frame = "geocentric"
+		self.v_bary = hdu[0].header["HIERARCH ESO QC VRAD BARYCOR"]
+
+	def __init__(self, filename):
+		self.filename = filename
+		self.load_spectrum()
+
+	def GetSpectrum (self):
+		return self.wave, self.flux
+
+
+sp = edibles_spectrum("/data/DIBs/data/EDIBLES/DR3/FITS/HD170740/RED_860/HD170740_w860_n20_20140916_L.fits")
+print("Barycentric Velocity is", sp.v_bary)
+wave,flux = sp.GetSpectrum()
+plt.plot(wave, flux)
+axes = plt.gca()
+axes.set_xlim([7660,7705])
+axes.set_ylim([0,160])
+plt.vlines((7667.021,7701.093), 0, 160, linestyles='dashed', colors='r')
+plt.show()
