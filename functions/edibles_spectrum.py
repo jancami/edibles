1aa597770c02497dabd200aa1266d43bf6bae4a0
Klay Kulik <klaykulik@gmail.com>
2019-09-16 08:08:22 -0400

    added barycentric correction to spectrum object

--- functions/edibles_spectrum.py
+++ functions/edibles_spectrum.py
@@ -1,5 +1,6 @@
 import numpy as np
 from astropy.io import fits
+import astropy.constants as cst
 import matplotlib.pyplot as plt
 from edibles.edibles_settings import *
 
@@ -20,6 +21,7 @@ class EdiblesSpectrum:
         self.wave_units = "AA"
         self.reference_frame = "geocentric"
         self.v_bary = hdu[0].header["HIERARCH ESO QC VRAD BARYCOR"]
+        self.bary_wave = self.wave + (self.v_bary/cst.c.to('km/s').value)*self.wave
 
     def __init__(self, filename):
         """
@@ -43,11 +45,12 @@ if __name__ == '__main__':
     filename = '/HD170740/RED_860/HD170740_w860_n20_20140916_L.fits'
     sp = EdiblesSpectrum(filename)
     print("Barycentric Velocity is", sp.v_bary)
-    wave,flux = sp.getSpectrum()
-    plt.plot(wave, flux)
+    plt.plot(sp.wave, sp.flux, label='Geocentric')
+    plt.plot(sp.bary_wave, sp.flux, label='Barycentric')
     axes = plt.gca()
     axes.set_xlim([7660,7705])
     axes.set_ylim([0,160])
     plt.vlines((7667.021,7701.093), 0, 160, linestyles='dashed', colors='r')
+    plt.legend()
     plt.show()
 
