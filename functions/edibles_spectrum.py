4911c63b0ca731b73ec84fc13a0d31ae5661be74
Klay Kulik <klaykulik@gmail.com>
2019-09-17 10:41:28 -0400

    added target name feature to spectrum obj.

--- functions/edibles_spectrum.py
+++ functions/edibles_spectrum.py
@@ -5,23 +5,25 @@ import matplotlib.pyplot as plt
 from edibles.edibles_settings import *
 
 class EdiblesSpectrum:
-# This object will contain a spectrum from EDIBLES, and a set of methods to operate on the data. 
+# This object will contain a spectrum from EDIBLES, 
+# and a set of methods to operate on the data. 
 
     def loadSpectrum (self):
         # Assume the file is a DR3 product here. 
         hdu = fits.open(self.filename)
         self.header = hdu[0].header
+        self.target = self.header["OBJECT"]
         self.date = self.header["DATE-OBS"]
         self.flux = hdu[0].data
         self.flux_units="arbitrary"
-        crval1 = hdu[0].header["CRVAL1"]
-        cdelt1 = hdu[0].header["CDELT1"]
+        crval1 = self.header["CRVAL1"]
+        cdelt1 = self.header["CDELT1"]
         nwave = len(self.flux)
         grid = np.arange(0, nwave, 1)
         self.wave = (grid) * cdelt1 + crval1
         self.wave_units = "AA"
         self.reference_frame = "geocentric"
-        self.v_bary = hdu[0].header["HIERARCH ESO QC VRAD BARYCOR"]
+        self.v_bary = self.header["HIERARCH ESO QC VRAD BARYCOR"]
         self.bary_wave = self.wave + (self.v_bary/cst.c.to('km/s').value)*self.wave
 
     def __init__(self, filename):
@@ -46,6 +48,7 @@ if __name__ == '__main__':
     filename = '/HD170740/RED_860/HD170740_w860_n20_20140916_L.fits'
     sp = EdiblesSpectrum(filename)
     print("Barycentric Velocity is", sp.v_bary)
+    print(sp.target)
     plt.plot(sp.wave, sp.flux, label='Geocentric')
     plt.plot(sp.bary_wave, sp.flux, label='Barycentric')
     axes = plt.gca()
