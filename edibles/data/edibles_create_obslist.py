5c7536bcdb77317744c1ce8d4ea41144556aaa11
Jan Cami <jancami@gmail.com>
2020-04-09 06:15:52 -0400

    Included echelle order (or ALL for merged) in Obs Log

--- edibles/data/edibles_create_obslist.py
+++ edibles/data/edibles_create_obslist.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import csv
-from edibles.edibles import DATADIR, EDIBLES_PYTHONDIR, DATARELEASE
+from edibles.edibles import DATADIR, PYTHONDIR, DATARELEASE
 from edibles.edibles.utils.edibles_spectrum import EdiblesSpectrum
 
 
@@ -12,14 +12,14 @@ def createObsList():
     to read the header and extract the information. Finally, store everything
     in a text file."""
 
-    print(DATADIR)
+    print("Data Release in " + DATADIR)
     allfitsfiles = []
     for path, dirs, files in os.walk(DATADIR):
         for file in files:
             if file.endswith(".fits"):
                 fullfilename = os.path.join(path, file)
-                relative_filename = fullfilename[len(DATADIR):]
-                print(relative_filename)
+                relative_filename = fullfilename[len(DATADIR) :]
+                # print(relative_filename)
                 allfitsfiles.append(relative_filename)
 
     print(len(allfitsfiles))
@@ -30,6 +30,7 @@ def createObsList():
             self.object = ""
             self.date_obs = ""
             self.setting = ""
+            self.order = ""
             self.wave_min = ""
             self.wave_max = ""
             self.ra = ""
@@ -42,8 +43,9 @@ def createObsList():
     full_list = [obsfile() for i in range(n_files)]
 
     for count in range(len(allfitsfiles)):
+        print(count, n_files, allfitsfiles[count])
         full_list[count].filename = allfitsfiles[count]
-        # print(datadir + full_list[count].filename)
+        # print(DATADIR + full_list[count].filename)
         spec = EdiblesSpectrum(full_list[count].filename)
         # print(spec.header)
         full_list[count].object = spec.header["OBJECT"]
@@ -51,19 +53,37 @@ def createObsList():
         full_list[count].ra = spec.header["RA"]
         full_list[count].dec = spec.header["DEC"]
         full_list[count].exptime = spec.header["EXPTIME"]
-        print(allfitsfiles[count])
+        idx_O = allfitsfiles[count].find("_O")
+        if idx_O != -1:
+            # print(idx)
+            idx_dot = allfitsfiles[count].find(".")
+            full_list[count].order = (allfitsfiles[count])[idx_O + 2 : idx_dot]
+        else:
+            full_list[count].order = "ALL"
         if "HIERARCH ESO INS GRAT1 WLEN" in spec.header:
             full_list[count].setting = int(spec.header["HIERARCH ESO INS GRAT1 WLEN"])
         if "HIERARCH ESO INS GRAT2 WLEN" in spec.header:
             full_list[count].setting = int(spec.header["HIERARCH ESO INS GRAT2 WLEN"])
-        wave, flux = spec.getSpectrum()
+        struct = spec.getSpectrum()
+        wave = struct["wave"]
+        flux = struct["flux"]
         full_list[count].wave_min = "{:.1f}".format(np.min(wave))
         full_list[count].wave_max = "{:.1f}".format(np.max(wave))
         del spec
 
     # Create arrays of formatted strings to print to a csv file now.
     pstrings = [
-        ["Object", "RA", "DEC", "DateObs", "setting", "WaveMin", "WaveMax", "Filename"]
+        [
+            "Object",
+            "RA",
+            "DEC",
+            "DateObs",
+            "Setting",
+            "Order",
+            "WaveMin",
+            "WaveMax",
+            "Filename",
+        ]
     ]
     for count in range(n_files):
         pstrings.append(
@@ -73,6 +93,7 @@ def createObsList():
                 full_list[count].dec,
                 full_list[count].date_obs,
                 full_list[count].setting,
+                full_list[count].order,
                 full_list[count].wave_min,
                 full_list[count].wave_max,
                 full_list[count].filename,
@@ -80,10 +101,10 @@ def createObsList():
         )
 
     # Time to print things out! Let's use csv format to do that.
-    outfile = EDIBLES_PYTHONDIR + "/data/" + DATARELEASE + "_ObsLog.csv"
+    outfile = PYTHONDIR + "/edibles/data/" + DATARELEASE + "_ObsLog.csv"
     length_checker = np.vectorize(len)
     all_lengths = length_checker(allfitsfiles)
-    print(np.max(all_lengths))
+    # print(np.max(all_lengths))
     with open(outfile, "w") as csvFile:
         writer = csv.writer(csvFile)
         writer.writerows(pstrings)
