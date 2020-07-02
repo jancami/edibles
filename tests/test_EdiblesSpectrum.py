fffc3cc9aba3a3f5f24dafc3c60d2e890ff5127e
Klay <klaykulik@gmail.com>
2020-07-02 16:36:17 -0400

    update tests to go along with EdiblesSpectrum.getSpectrum update

--- tests/test_EdiblesSpectrum.py
+++ tests/test_EdiblesSpectrum.py
@@ -1,5 +1,6 @@
 import pandas
 import astropy
+import pytest
 import numpy as np
 from edibles.utils.edibles_spectrum import EdiblesSpectrum
 
@@ -13,27 +14,28 @@ def testEdiblesSpectrum(filename="tests/HD170740_w860_redl_20140915_O12.fits"):
     assert isinstance(sp.v_bary, float)
 
     assert isinstance(sp.df, pandas.core.frame.DataFrame)
-    assert isinstance(sp.wave, pandas.core.frame.Series)
+    print(type(sp.wave))
+    assert isinstance(sp.wave, np.ndarray)
     assert isinstance(sp.wave_units, str)
-    assert isinstance(sp.bary_wave, pandas.core.frame.Series)
-    assert isinstance(sp.flux, pandas.core.frame.Series)
+    assert isinstance(sp.bary_wave, np.ndarray)
+    assert isinstance(sp.flux, np.ndarray)
     assert isinstance(sp.flux_units, str)
 
-    subset1 = sp.getSpectrum()
-    assert isinstance(subset1, pandas.core.frame.DataFrame)
-    assert isinstance(subset1.wave, pandas.core.frame.Series)
-    assert isinstance(subset1.flux, pandas.core.frame.Series)
-    assert sp.df.equals(subset1)
+
+    with pytest.raises(AssertionError):
+        assert sp.getSpectrum()
 
     xmin = 7660
     xmax = 7680
-    subset2 = sp.getSpectrum(xmin=xmin, xmax=xmax)
-    assert isinstance(subset2, pandas.core.frame.DataFrame)
-    assert isinstance(subset2.wave, pandas.core.frame.Series)
-    assert isinstance(subset2.flux, pandas.core.frame.Series)
+    subset = sp.getSpectrum(xmin=xmin, xmax=xmax)
+    assert isinstance(sp.df, pandas.core.frame.DataFrame)
+    assert isinstance(sp.wave, np.ndarray)
+    assert isinstance(sp.flux, np.ndarray)
+    assert isinstance(subset, pandas.core.frame.DataFrame)
 
-    assert np.min(subset2.wave) > xmin
-    assert np.max(subset2.wave) < xmax
+    assert np.min(sp.wave) > xmin
+    assert np.max(sp.wave) < xmax
+
 
 
 if __name__ == "__main__":
