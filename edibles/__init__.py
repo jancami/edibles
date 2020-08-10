f90c6c27b39fc9cea54a23992173b9b8f0e75b18
Klay <klaykulik@gmail.com>
2020-08-10 15:06:24 -0400

    update PYTHONDIR

--- edibles/__init__.py
+++ edibles/__init__.py
@@ -14,8 +14,6 @@ else:
 if 'EDIBLES_PYTHONDIR' in os.environ:
     PYTHONDIR = os.environ['EDIBLES_PYTHONDIR'] + '/edibles'
 else:
-    PYTHONDIR = os.path(__file__)
-
-print(os.path.dirname(__file__))
+    PYTHONDIR = os.path.dirname(__file__)
 
 __version__ = '0.1'
