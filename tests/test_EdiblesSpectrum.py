403ad7ed4713ed8c3b4c7433ec8d9cdb72662bad
Klay <klaykulik@gmail.com>
2020-08-10 11:02:51 -0400

    update testing for EdiblesSpectrum

--- tests/test_EdiblesSpectrum.py
+++ tests/test_EdiblesSpectrum.py
@@ -30,12 +30,12 @@ def testEdiblesSpectrum(filename="tests/HD170740_w860_redl_20140915_O12.fits"):
     assert isinstance(sp.raw_sky_flux, np.ndarray)
     assert len(sp.raw_sky_wave) == len(sp.raw_sky_flux)
 
-    # getSpectrum
     assert isinstance(sp.wave, np.ndarray)
     assert isinstance(sp.bary_wave, np.ndarray)
     assert isinstance(sp.flux, np.ndarray)
 
 
+    # getSpectrum
     xmin = 7660
     xmax = 7680
     sp.getSpectrum(xmin=xmin, xmax=xmax)
@@ -67,8 +67,41 @@ def testEdiblesSpectrum(filename="tests/HD170740_w860_redl_20140915_O12.fits"):
     assert np.min(sp.sky_wave) > sp.xmin
     assert np.max(sp.sky_wave) < sp.xmax
 
+    # shift
+    zoom_xmin = 7661
+    zoom_xmax = 7679
+    shift = 0.05
+    sp.shift(shift=shift, zoom_xmin=zoom_xmin, zoom_xmax=zoom_xmax)
 
-    sp.shift()
+    assert isinstance(sp.wave, np.ndarray)
+    assert isinstance(sp.flux, np.ndarray)
+    assert len(sp.wave) == len(sp.flux)
+    assert np.min(sp.wave) > sp.xmin
+    assert np.max(sp.wave) < sp.xmax
+
+    assert isinstance(sp.bary_wave, np.ndarray)
+    assert isinstance(sp.bary_flux, np.ndarray)
+    assert len(sp.bary_wave) == len(sp.bary_flux)
+    assert np.min(sp.bary_wave) > sp.xmin
+    assert np.max(sp.bary_wave) < sp.xmax
+
+    assert isinstance(sp.grid, np.ndarray)
+    assert isinstance(sp.interp_flux, np.ndarray)
+    assert isinstance(sp.interp_bary_flux, np.ndarray)
+    assert len(sp.grid) == len(sp.interp_flux)
+    assert len(sp.grid) == len(sp.interp_bary_flux)
+    assert np.min(sp.grid) > sp.xmin
+    assert np.max(sp.grid) < sp.xmax
+
+    assert isinstance(sp.sky_wave, np.ndarray)
+    assert isinstance(sp.sky_flux, np.ndarray)
+    assert len(sp.sky_wave) == len(sp.sky_flux)
+    assert np.min(sp.sky_wave) > sp.xmin
+    assert np.max(sp.sky_wave) < sp.xmax
+
+
+
+
 
 
 
