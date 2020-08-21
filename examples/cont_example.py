0a5a24f5b11d8914c32306f91f55ce09de773e28
Klay <klaykulik@gmail.com>
2020-08-21 15:08:07 -0400

    update cont_example

--- examples/cont_example.py
+++ examples/cont_example.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from edibles.models import ContinuumModel
 
+
 # #################################################################################
 # Example 1
 
@@ -12,40 +13,64 @@ y = x**3 - 3 * x**2 + 1
 cont_model = ContinuumModel(n_anchors=4)
 cont_pars = cont_model.guess(y, x=x)
 
+
 # ##############################
 # Show initial model
 
 out = cont_model.eval(data=y, params=cont_pars, x=x)
 
-init_pars = [cont_pars['y_0'].value,
-             cont_pars['y_1'].value,
-             cont_pars['y_2'].value,
-             cont_pars['y_3'].value]
-init_x = np.linspace(0, 3, 4)
+y_param_names = []
+for i in range(cont_model.n_anchors):
+    y_param_names.append('y_' + str(i))
+
+x_param_names = []
+for i in range(cont_model.n_anchors):
+    x_param_names.append('x_' + str(i))
+
+init_y = []
+for par_name in y_param_names:
+    init_y.append(cont_pars[par_name].value)
+
+init_x = []
+for par_name in x_param_names:
+    init_x.append(cont_pars[par_name].value)
 
 plt.scatter(x, y)
 plt.plot(x, out, 'C1')
-plt.scatter(init_x, init_pars, marker='x', s=80, color='k')
+plt.scatter(init_x, init_y, marker='x', s=80, color='k')
 plt.show()
 
+
 # ##############################
-# Fit & show results
+# Fit
 
 result = cont_model.fit(data=y, params=cont_pars, x=x)
 out = cont_model.eval(data=y, params=result.params, x=x)
-resid = y - out
+# print(result.fit_report())
 
 
-print(result.fit_report())
+# ##############################
+# Show results
+
+result_y_pars = []
+for par_name in y_param_names:
+    result_y_pars.append(result.params[par_name].value)
+
+result_x_pars = []
+for par_name in x_param_names:
+    result_x_pars.append(result.params[par_name].value)
 
 result.plot_fit()
-plt.scatter(init_x, result.params, marker='x', color='r', s=80, zorder=10, label='Fit params')
-plt.scatter(init_x, init_pars, marker='x', s=80, color='k', label='Initial params')
+plt.scatter(result_x_pars, result_y_pars, marker='x',
+            color='r', s=80, zorder=10, label='Fit params')
+plt.scatter(init_x, init_y, marker='x', s=80, color='k', label='Initial params')
 plt.legend()
 plt.show()
 
+
 # #################################################################################
 # Example 2
+# #################################################################################
 
 x = np.linspace(0, 8)
 y = np.sin(x)
@@ -53,36 +78,56 @@ y = np.sin(x)
 cont_model = ContinuumModel(n_anchors=5)
 cont_pars = cont_model.guess(y, x=x)
 
+
 # ##############################
 # Show initial model
 
 out = cont_model.eval(data=y, params=cont_pars, x=x)
 
-init_pars = [cont_pars['y_0'].value,
-             cont_pars['y_1'].value,
-             cont_pars['y_2'].value,
-             cont_pars['y_3'].value,
-             cont_pars['y_4'].value]
+y_param_names = []
+for i in range(cont_model.n_anchors):
+    y_param_names.append('y_' + str(i))
 
-init_x = np.linspace(0, 8, 5)
+x_param_names = []
+for i in range(cont_model.n_anchors):
+    x_param_names.append('x_' + str(i))
+
+init_y = []
+for par_name in y_param_names:
+    init_y.append(cont_pars[par_name].value)
+
+init_x = []
+for par_name in x_param_names:
+    init_x.append(cont_pars[par_name].value)
 
 plt.scatter(x, y)
 plt.plot(x, out, 'C1')
-plt.scatter(init_x, init_pars, marker='x', s=80, color='k')
+plt.scatter(init_x, init_y, marker='x', s=80, color='k')
 plt.show()
 
+
 # ##############################
-# Fit & show results
+# Fit
 
 result = cont_model.fit(data=y, params=cont_pars, x=x)
 out = cont_model.eval(data=y, params=result.params, x=x)
-resid = y - out
+# print(result.fit_report())
 
 
-print(result.fit_report())
+# ##############################
+# Show results
+
+result_y_pars = []
+for par_name in y_param_names:
+    result_y_pars.append(result.params[par_name].value)
+
+result_x_pars = []
+for par_name in x_param_names:
+    result_x_pars.append(result.params[par_name].value)
 
 result.plot_fit()
-plt.scatter(init_x, result.params, marker='x', color='r', s=80, zorder=10, label='Fit params')
-plt.scatter(init_x, init_pars, marker='x', s=80, color='k', label='Initial params')
+plt.scatter(result_x_pars, result_y_pars, marker='x',
+            color='r', s=80, zorder=10, label='Fit params')
+plt.scatter(init_x, init_y, marker='x', s=80, color='k', label='Initial params')
 plt.legend()
 plt.show()
