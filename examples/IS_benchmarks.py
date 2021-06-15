e32468ad8f9db9a96c39883f6271a217991815bf
Jan Cami <jcami@uwo.ca>
2021-06-15 13:52:53 -0400

    benchmark skeleton

---
+++ examples/IS_benchmarks.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pathlib import Path
+from edibles import PYTHONDIR
+from edibles.utils.ISLineFitter import ISLineFitter
+
+# Load the benchmark data and run it through ISLineFitter. Then compare results. 
+
+this_datadir = Path(PYTHONDIR) / "data" / "voigt_benchmarkdata"
+filename = this_datadir / "omiper.m95.7698.txt"
+v_resolution = 0.56 # km/s
+#print(filename)
+arrays = np.genfromtxt(filename,skip_header=1)
+
+wave = arrays[:,0]
+flux = arrays[:,1]
+#print(wave, flux)
+
+fitter = ISLineFitter(wave,flux, v_resolution=0.56)
+#KI_params = fitter.fit(species='KI')
+#print(KI_params)
