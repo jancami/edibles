009201d7f5f0ad7df3d577b3773a599e1f411322
Missael Barco <missael_gabo@hotmail.com>
2021-06-28 10:35:46 -0500

    First version of documentation and comments

--- edibles/utils/simulations/RotationalEnergies.py
+++ edibles/utils/simulations/RotationalEnergies.py
@@ -1,10 +1,4 @@
-"""A one line summary of the module or program, terminated by a period.
-
-Leave one blank line.  The rest of this docstring should contain an
-overall description of the module or program.  Optionally, it may also
-contain a brief description of exported classes and functions and/or usage
-examples.
-"""
+"""Module to simulate the PQR- branches of a molecule with known symmetry."""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -39,7 +33,7 @@ class Rotational_Energies:
         A (float): Rotational constant of the first rotational axis.
         B (float): Constant of the second axis.
         C (float): Constant of the third axis.
-        Name (str): Name of the DIB line.
+        Name (str): Name of sigthline dataset. 
         Q_scale (float): Scale of the Q-branch.
         PR_scale (TYPE): Scale of the P-branch and R-branch.
     """
@@ -58,6 +52,7 @@ class Rotational_Energies:
     def _determine_symmetry_type(self):
         """Get symmetry type accordingly with the rotational constants."""
         self.flag = False
+
         # An spherical symmetry has the same rotational constant for all axes.
         if self.A == self.B == self.C:
             self.symmetry_type = 'spherical'
@@ -92,8 +87,10 @@ class Rotational_Energies:
         """
         # print("Calculating rotational energy values")
         self.Jlimit = Jlimit
+
         if self.symmetry_type == 'spherical':
             df = pd.DataFrame(columns=["J", "E"])
+
             # Values of the quantum rotational number.
             J_vals = list(range(0, self.Jlimit+1))
 
@@ -105,14 +102,17 @@ class Rotational_Energies:
 
         else:
             df = pd.DataFrame(columns=["J", "K"])
+
             # Values of the first quantum rotational number J.
             J_vals = list(range(0, self.Jlimit+1))
 
             for J in J_vals:
                 # Values of the second rotational number K.
+
                 for K in list(range(-J, J+1)):
                     # Combinations of J and K values
                     df = df.append({"J": J, "K": K}, ignore_index=True)
+
             # Energy of each (J, K) pair of a prolate symmetry.
             if self.symmetry_type == 'symmetric_prolate':
                 df["E"] = self.B*df["J"]*(df["J"]+1)+(self.A-self.B)*df["K"]**2
@@ -162,7 +162,7 @@ class Rotational_Energies:
                 This array must comes from <Rotational_Energies>.J
             Kup (1darray): Values of the upper states of the K rotational number.
             Eup (1darray): Values of the upper states of energy.
-            Q_branch (bool): Optional; the default is False. Wheter to consider
+            Q_branch (bool, optional): Default to False. Wheter to consider
                 or not the Q-branch.
         """
         df = pd.concat([self.J, self.K, self.E, self.population], axis=1)
@@ -284,6 +284,7 @@ class Rotational_Energies:
             self.allowed_combo_data['nJ'].loc[self.allowed_combo_data['DeltaJ'] != 0] = \
                 self.allowed_combo_data["nJ"]*self.PR_scale
 
+            # Intensity proportional to population.
             self.transition_intensity = self.allowed_combo_data["nJ"]
 
             # Find highest populated state.
@@ -291,47 +292,78 @@ class Rotational_Energies:
             self.highest_pop_state = (self.allowed_combo_data["J"].iloc[max_pop_idx])
 
             # print("Jmax=" ,self.highest_pop_state)
-# TO-DO
+
     def plot_transitions(self):
-        """Plot the resulting transitions"""
+        """Plot the resulting transitions."""
         # print("Plotting the resulting transitions")
+
+        # Plot every transition line (size equivalent to intensity).
         plt.vlines(x=self.transition_freqs, ymax=self.transition_intensity,
                    ymin=0, color='black', label='transitions')
 
+        # DataFrame of transitions.
         df = pd.DataFrame({"Trans_Freqs": self.transition_freqs,
                            "Trans_Intens": self. transition_intensity,
                            "Delta J": self.allowed_combo_data["J'"] -
                            self.allowed_combo_data["J"]})
 
+        # Transitions sorted by frequency.
         df = df.sort_values(by=["Trans_Freqs"])
         self.sorted_freqs = df["Trans_Freqs"]
         self.sorted_intens = df["Trans_Intens"]
 
+        # Plot details.
         plt.xlabel("Transition Frequency (1/cm)")
         plt.ylabel("Intensity")
         plt.title(str(self.name))
         plt.savefig(str(self.name)+'_'+self.symmetry_type+".pdf")
 
     def _rebin_data(self, X, Y, bin_size, Verbose=False):
+        """Resample data with a given interval.
 
+        Args:
+            X (1darray): Data of the X axis.
+            Y (1darray): Data of the Y axis.
+            bin_size (float): Interval for resampling.
+            Verbose (bool, optional): Default to False. Wheter or not to 
+                describe progress of execution.
+
+        Returns: 
+            rebinned_x (1darray): Resampled X data.
+            rebinned_y (1darray): Resampled Y data.
+        """
         rebinned_x, rebinned_y = [], []
+
+        # Internvals to resample X data.
         bins = np.arange(np.float(np.min(X))-5,
                          np.float(np.max(X))+5, bin_size)
 
+        # Classification of X data in bins.
         inds = np.digitize(X, bins)
 
+        # DataFrame for saving results.
         df2 = pd.DataFrame({"X": X, "Y": Y, "bin": inds})
 
+        # For every bin.
         for i in range(1, len(bins)):
 
+            # Elements in the i-th bin.
             search = df2.loc[df2["bin"] == i]
 
+            # If not empty.
             if len(search.index) != 0:
+
+                # Center of bin.
                 center_wave = bins[i]-bin_size/2
+
+                # Intensity of bin.
                 sum_y = search["Y"].sum()
 
+                # Save results of iteration.
                 rebinned_x.append(center_wave)
                 rebinned_y.append(sum_y/bin_size)
+
+                # Describe progress.
                 if Verbose:
                     print('Bin:', i, 'from', bins[i-1], 'to', bins[i])
                     print('Wavenlength:', bins[i]-bin_size/2)
@@ -340,53 +372,76 @@ class Rotational_Energies:
                     print('Y To Plot:', sum_y/bin_size)
                     print('---------')
 
+            # If empty, just assigne zero intensity to bin.
             else:
                 center_wave = bins[i]-bin_size/2
                 rebinned_x.append(center_wave)
                 rebinned_y.append(0)
 
+        # Return rebinned data.
         return(rebinned_x, rebinned_y)
 
     def _CreateWave(self, X, Y, lambda0):
+        """Transform and resample data to final contour data.
+
+        Args:
+            X (1darray): X data array. Must have 1/cm units.
+            Y (1darray): y data array.
+            lambda0 (float): Center wavelength of DIB. (Angstrom)
+
+        Returns:
+            Final_X (1darray): Final transformed X data.
+            Y_sampled (1darray): Final transformed Y data.
+        """
+        # Some constants.
         c = const.c.to('km/s')
         kms = (u.km / u.s)
 
-    # convert X into velocity space. del_sigma is measured in 1/cm
-    # (built into rotational_energies class) and lambda0 is measured in AA.
+        # convert X into velocity space.
         V = [((c*(del_sigma*u.k*lambda0*u.AA)).to(u.km / u.s)).value for del_sigma in X]
-        #################################################################
+
         # resample V and Y at 0.1km/s intervals
         bin_size = 0.1
         X_sampled, Y_sampled = self._rebin_data(V, Y, bin_size)
 
-        #################################################
         # Convert velocities back into wavelengths
-
         Final_X = [((lambda0*u.AA*(1+(V*kms/c))).to(u.AA)).value for V in X_sampled]
 
         return(Final_X, Y_sampled)
 
     def apply_voigt(self, lambda0, show_figure=False):
-        """Summary of method here...
+        """Apply voigt profile to frequencies and intensities.
 
-        Longer method information...
+        Apply a voig profile (convolution of Cauchy-Lorentz and Gaussian
+        distributions) to model the (frecuency, intensity) class data.
 
         Args:
-            lambda0 (TYPE): DESCRIPTION
-            show_figure (TYPE): Optional; the default is False. DESCRIPTION
+            lambda0 (float): Center wavelenght of DIB. (Angstroms).
+            show_figure (bool): Default to False. Wheter or not to show the
+                resulting figure.
         """
+        # Get data from class.
         X = self.transition_freqs
         Y = self.transition_intensity
+
+        # Get wave from data.
         Wave, Intensity = self._CreateWave(X, Y, lambda0)
+
+        # Array for final taus of voigt profile.
         final_tau = np.zeros(len(Wave))
 
+        # For every data point.
         for i in range(len(Wave)):
+            # Apply voigt optical depth.
             tau = voigt_optical_depth(Wave, lambda0=Wave[i], b=1, N=Intensity[i]*10**10,
                                       f=1, gamma=1e7, v_rad=0)
             final_tau = final_tau+tau
+
+        # Save results in class.
         self.spectrax = Wave
         self.spectray = final_tau
 
+        # Plot results.
         if show_figure:
             plt.plot(Wave, final_tau, 'k-', label='voigt profile applied')
             plt.xlabel('Wavelength $\mathrm{\AA}$')
@@ -394,15 +449,16 @@ class Rotational_Energies:
             plt.show()
 
     def apply_radiative_transfer(self, show_figure=False):
-        """Summary of method here...
-
-        Longer method information...
+        """Apply radiative transfer to data from voigt profile.
 
         Args:
-            show_figure (TYPE): Optional; the defalut is False. DESCRIPTION
+            show_figure (bool, optional): Defalut is False. Wheter or not to show the
+                resulting figure.
         """
         self.simple_rt_y = 1-self.spectray
         self.full_rt_y = np.exp(-self.spectray)
+
+        # Plot results.
         if show_figure:
             plt.plot(self.spectrax, self.simple_rt_y, 'k-',
                      label='after radiative transfer step')
@@ -413,28 +469,32 @@ class Rotational_Energies:
         return()
 
     def smooth_spectra(self, lambda0, show_figure=False):
-        """Summary of method here...
+        """Smooth spectra.
 
-        Longer method information...
+        Smooth spectra via a 1D Gaussian Kernel.
 
         Args:
-            lambda0  (TYPE): DESCRIPTION
-            show_figure (TYPE): Optional; the defalut is False. DESCRIPTION
+            lambda0  (float): Center wavelenght of DIB. (Angstroms).
+            show_figure (bool, optional): Defalut is False. Wheter or not to show the
+                resulting figure.
         """
         dx = np.asarray(self.spectrax[1:])-np.asarray(self.spectrax[0:-1])
         d_lambda = lambda0/80000
         sigma_a = d_lambda/2.355
         sigma_p = sigma_a/dx[0]
 
+        # Smooth data through a 1d Gaussian kernel.
         kernal = Gaussian1DKernel(sigma_p)
         convolved_y = convolve(self.full_rt_y, kernal, boundary='extend')
 
+        # Plot results.
         if show_figure:
             plt.plot(self.spectrax, convolved_y/np.max(convolved_y),
                      'c-', label='after smoothing')
             plt.xlabel('Wavelength $\mathrm{\AA}$')
             plt.ylabel('Normalized Intensity')
 
+        # Save results in class.
         self.final_y = convolved_y/np.max(convolved_y)
 
         return()
