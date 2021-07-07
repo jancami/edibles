d03aaa118f4d786442960d40f300959690403d4b
Missael Barco <missael_gabo@hotmail.com>
2021-07-07 18:55:20 -0500

    Star function for parameter survey with two variables

--- edibles/utils/simulations/SimulatedContourParameterSurvey.py
+++ edibles/utils/simulations/SimulatedContourParameterSurvey.py
@@ -171,7 +171,7 @@ def one_variable_survey(path, B_values, T_values, delta_values, Q_Branch=True,
     Sightline = 'ParameterSurvey'
     Q_scale_init = 1
 
-    # Dictionary to save simulations per parameter
+    # Dictionary to save simulations per parameter varying.
     param_simulations = {'B': [], 'T': [], 'delta': []}
 
     # Dictionary to save parameters used for simulation
@@ -243,15 +243,135 @@ def one_variable_survey(path, B_values, T_values, delta_values, Q_Branch=True,
                 pickle.dump([param_simulations[param], parameters[param]], output)
 
 
+def assign_two_variables(value1, value2, name1, name2,
+                         B_default, T_default, delta_default):
+    """Assign parameters values in the two-parameter survey.
+
+    Args:
+        value1 (float): Value of the first variable.
+        value2 (float): Value of the second variable.
+        name1 (str): Name of first variable (B, T or delta).
+        name2 (str): Name of second variable.
+        B_default (float): Default value of B when constant.
+        T_default (float): Default value of T.
+        delta_default (float): Default value of delta.
+
+    Returns:
+        B (float): Final value of B
+        T (float): Value of T.
+        delta (float): Value of delta.
+    """
+    if name1 == 'B':
+        B = value1
+        if name2 == 'T':
+            T = value2
+            delta = delta_default
+
+        elif name2 == 'delta':
+            delta = value2
+            T = T_default
+
+    elif name1 == 'T':
+        T = value1
+        if name2 == 'B':
+            B = value2
+            delta = delta_default
+
+        elif name2 == 'delta':
+            delta = value2
+            B = B_default
+
+    elif name1 == 'delta':
+        delta = value1
+        if name2 == 'B':
+            B = value2
+            T = T_default
+
+        elif name2 == 'T':
+            T = value2
+            B = B_default
+
+    return B, T, delta
+
+
+def two_variable_survey(path, B_values, T_values, delta_values, Q_Branch=True,
+                        B_default=0.02, T_default=10, delta_default=0, save=False, load=False):
+
+    # Declare constants
+    SymmetryType = 'Spherical'
+    Jlimit = 200
+    lambda0 = 6614
+    Sightline = 'ParameterSurvey'
+    Q_scale = 1
+
+    # Dictionary to save simulations
+    param_simulations = {'BT': {},
+                         'Tdelta': {},
+                         'Bdelta': {}}
+
+    # Dictionary to save parameters used in simulations.
+    parameters = {'BT': {},
+                  'Tdelta': {},
+                  'Bdelta': {}}
+
+    # Dictionary with parameter space
+    param_values = {'BT': {'B': B_values, 'T': T_values},
+                    'Tdelta': {'T': T_values, 'delta': delta_values},
+                    'Bdelta': {'B': B_values, 'delta': delta_values}}
+
+    for combinations in param_values.keys():
+        # Load pre-calculated simulations
+        if load:
+            with open(f"{path}/{SymmetryType}_Changing{combinations}_.bin", "rb") as data:
+                # Extract data
+                build = pickle.load(data)
+                param_simulations[combinations] = build[0]
+                parameters[combinations] = build[1]
+
+        # Run simulation.
+        else:
+
+            variables = list(param_values[combinations].keys())
+
+            # Simulation
+            for value1 in param_values[combinations][variables[0]]:
+
+                param_simulations[combinations][value1] = {}
+                parameters[combinations][value1] = {}
+                for value2 in param_values[combinations][variables[1]]:
+
+                    B, T, delta = assign_two_variables(value1, value2, variables[0], variables[1],
+                                                       B_default, T_default, delta_default)
+                    A = C = B
+
+                    build = sim(A=A, B=B, C=C, Delta_A=delta*A, Delta_B=delta*B, Delta_C=delta*C,
+                                Trot=T, Jlimit=Jlimit, Target=Sightline, lambda0=lambda0,
+                                Q_Branch=Q_Branch, Q_scale=Q_scale)
+
+                    # Obtain wavenumbers.
+                    x_vals = WavelengthToWavenumber(np.asarray(build[0]))
+                    y_vals = build[1]
+
+                    # Save results and parameters.
+                    param_simulations[combinations][value1][value2] = {
+                        'simulation': [x_vals, y_vals], 'parameters': [B, T, delta]}
+                    parameters[combinations][value1][value2] = [B, T, delta]
+
+        # Save simulation to future reference.
+        if save:
+            with open(f"{path}/{SymmetryType}_Changing{combinations}_.bin", "wb") as output:
+                pickle.dump([param_simulations[combinations], parameters[combinations]], output)
+
+
 if __name__ == "__main__":
 
     # Define parameter ranges.
-    T_values = np.linspace(10, 100, 10)
-    B_values = 10**np.linspace(-4, -1, 10)
-    delta_values = np.linspace(0, 0.05, 10)
+    T_values = np.linspace(10, 100, 3)
+    B_values = 10**np.linspace(-4, -1, 3)
+    delta_values = np.linspace(0, 0.05, 3)
 
     # Run simulations with and without the Q-branch
-    one_variable_survey('Parameter_survey/QTrue', B_values, T_values,
-                        delta_values, load=True)
-    one_variable_survey('Parameter_survey/QFalse',  B_values, T_values,
-                        delta_values, Q_Branch=False, load=True)
+    # one_variable_survey('Parameter_survey/QTrue', B_values, T_values, delta_values, load=True)
+    # one_variable_survey('Parameter_survey/QFalse',  B_values, T_values, delta_values, Q_Branch=False, load=True)
+
+    two_variable_survey('Parameter_survey/2D/QTrue', B_values, T_values, delta_values, save=True)
