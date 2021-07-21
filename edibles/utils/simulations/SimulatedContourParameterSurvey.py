95bd7e2199c3ed7f3944e9be6aee929e81694dc0
Missael Barco <missael_gabo@hotmail.com>
2021-07-21 16:54:53 -0500

    Add A and C to one variable survey method

--- edibles/utils/simulations/SimulatedContourParameterSurvey.py
+++ edibles/utils/simulations/SimulatedContourParameterSurvey.py
@@ -112,7 +112,7 @@ def animation(simulations, param_name, param_values, SymmetryType, lambda0,
 
 
 def plot_simulations(simulations, param_name, param_values,
-                     SymmetryType, lambda0, path='Test_data'):
+                     SymmetryType, lambda0, path='Test_data', save=False):
     """Plot a set of simulations.
 
     Plot in a single graph a set of simulations obtained by varying some
@@ -136,33 +136,39 @@ def plot_simulations(simulations, param_name, param_values,
                  f'Delta: {param_values[i][2]:.3f}', color=colours[i])
 
     # Plot style
-    a = WavelengthToWavenumber(lambda0)
-    plt.xlim((a-10, a+10))
+    plt.xlim((-10, 10))
 
     plt.xlabel(r'Wavenumber ($cm^{-1}$)')
     plt.ylabel('Normalized Intensity')
     plt.legend(fontsize='x-small')
     plt.title('Parameter Survey - Changing '+param_name)
-    plt.savefig(f"{path}/"+str(SymmetryType)+"_Changing"+param_name+".pdf", dpi=300)
+
+    if save:
+        plt.savefig(f"{path}/"+str(SymmetryType)+"_Changing"+param_name+".pdf", dpi=300)
     plt.show()
     plt.close()
 
 
-def one_variable_survey(path, B_values, T_values, delta_values, Q_Branch=True,
-                        B_default=0.02, T_default=10, delta_default=0, save=False, load=False):
+def one_variable_survey(path, A_values=None, B_values=None, C_values=None, T_values=None,
+                        delta_values=None, Q_Branch=True, B_default=0.02, T_default=10,
+                        delta_default=0, save=False, load=False, save_fig=False, anim=False):
     """Perform a parameter survey over individual parameters in a Spherical Symmetry.
 
     Generates plots and GIFs of surveys performed over individual parameters.
-    (T, B and delta).
+    (T, A, B, C, and delta).
 
     Args:
-        B_values (1darray): Array with values of B.
-        T_values (1darray): Array with values of T.
-        delta_values (1darray): Array with values of deltaB.
+        A_values (1darray): Array with values of A. Ignored if empty. Defaults to None.
+        B_values (1darray): Array with values of B. Ignored if empty. Defaults to None.
+        C_values (1darray): Array with values of C. Ignored if empty. Defaults to None.
+        T_values (1darray): Array with values of T. Ignored if empty. Defaults to None.
+        delta_values (1darray): Array with values of deltaB. Ignored if empty.  Defaults to None.
         Q_Branch (bool, optional): To consider or not the Q-branch. Defaults to True.
-        B_default (flota, optional): Default value of B when constant. Defaults to 0.02
-        T_default (flota, optional): Defaults to 10
-        B_default (flota, optional): Defaults to 0.0
+        B_default (float, optional): Default value of B when constant. Defaults to 0.02
+        T_default (float, optional): Defaults to 10
+        B_default (float, optional): Defaults to 0.0
+        save_fig (bool, optional): Defaults to False.
+        anim (bool, optional): Create GIF or not. Defaults to False.
     """
     # Declare constants
     SymmetryType = 'Spherical'
@@ -172,75 +178,93 @@ def one_variable_survey(path, B_values, T_values, delta_values, Q_Branch=True,
     Q_scale_init = 1
 
     # Dictionary to save simulations per parameter varying.
-    param_simulations = {'B': [], 'T': [], 'delta': []}
+    param_simulations = {'A': [], 'B': [], 'C': [], 'T': [], 'delta': []}
 
     # Dictionary to save parameters used for simulation
-    parameters = {'B': [], 'T': [], 'delta': []}
+    parameters = {'A': [], 'B': [], 'C': [], 'T': [], 'delta': []}
 
     # Dictionary with parameter space
-    param_values = {'B': B_values, 'T': T_values, 'delta': delta_values}
+    param_values = {'A': A_values, 'B': B_values, 'C': C_values,
+                    'T': T_values, 'delta': delta_values}
 
     # Varying all the parameters.
     for param in param_values.keys():
+        if param_values[param] is not None:
+            # Load pre-calculated simulations
+            if load:
+                with open(f"{path}/{SymmetryType}_Changing{param}_.bin", "rb") as data:
+                    # Extract data
+                    build = pickle.load(data)
+                    param_simulations[param] = build[0]
+                    parameters[param] = build[1]
 
-        # Load pre-calculated simulations
-        if load:
-            with open(f"{path}/{SymmetryType}_Changing{param}_.bin", "rb") as data:
-                # Extract data
-                build = pickle.load(data)
-                param_simulations[param] = build[0]
-                parameters[param] = build[1]
+            # Run simulation.
+            else:
+                # Iterate over values of parameter.
+                for i in range(len(param_values[param])):
 
-        # Run simulation.
-        else:
-            # Iterate over values of parameter.
-            for i in range(len(param_values[param])):
+                    # Initializate Q_scale
+                    Q_scale = Q_scale_init
 
-                # Initializate Q_scale
-                Q_scale = Q_scale_init
+                    # Parameter values.
+                    if param == 'A':
+                        B = B_default
+                        C = B
+                        A = A_values[i]
+                        T = T_default
+                        delta = delta_default
 
-                # Parameter values.
-                if param == 'B':
-                    B = B_values[i]
-                    A = C = B
-                    T = T_default
-                    delta = delta_default
+                    if param == 'B':
+                        B = B_values[i]
+                        A = C = B
+                        T = T_default
+                        delta = delta_default
 
-                elif param == 'T':
-                    B = B_default
-                    A = C = B
-                    T = T_values[i]
-                    delta = delta_default
+                    if param == 'C':
+                        B = B_default
+                        A = B
+                        C = C_values[i]
+                        T = T_default
+                        delta = delta_default
 
-                elif param == 'delta':
-                    B = B_default
-                    A = C = B
-                    T = T_default
-                    delta = delta_values[i]
+                    elif param == 'T':
+                        B = B_default
+                        A = C = B
+                        T = T_values[i]
+                        delta = delta_default
 
-                # Run simulation
-                build = sim(A=A, B=B, C=C, Delta_A=delta*A, Delta_B=delta*B, Delta_C=delta*C,
-                            Trot=T, Jlimit=Jlimit, Target=Sightline, lambda0=lambda0,
-                            Q_Branch=Q_Branch, Q_scale=Q_scale)
+                    elif param == 'delta':
+                        B = B_default
+                        A = C = B
+                        T = T_default
+                        delta = delta_values[i]
 
-                # Obtain wavenumbers.
-                x_vals = WavelengthToWavenumber(np.asarray(build[0]))
-                y_vals = build[1]
+                    # Run simulation
+                    build = sim(A=A, B=B, C=C, Delta_A=delta*A, Delta_B=delta*B, Delta_C=delta*C,
+                                Trot=T, Jlimit=Jlimit, Target=Sightline, lambda0=lambda0,
+                                Q_Branch=Q_Branch, Q_scale=Q_scale)
 
-                # Save results and parameters.
-                param_simulations[param].append([x_vals, y_vals])
-                parameters[param].append([B, T, delta])
+                    # Obtain wavenumbers.
+                    x_vals = WavelengthToWavenumber(np.asarray(build[0])) - \
+                        WavelengthToWavenumber(lambda0)
+                    y_vals = build[1]
 
-        # Generates GIF and plot.
-        plot_simulations(param_simulations[param], param,
-                         parameters[param], SymmetryType, lambda0, path=path)
-        animation(param_simulations[param], param, parameters[param],
-                  SymmetryType, lambda0, path=path)
+                    # Save results and parameters.
+                    param_simulations[param].append([x_vals, y_vals])
+                    parameters[param].append([B, T, delta])
 
-        # Save simulation to future reference.
-        if save:
-            with open(f"{path}/{SymmetryType}_Changing{param}_.bin", "wb") as output:
-                pickle.dump([param_simulations[param], parameters[param]], output)
+            # Generates GIF and plot.
+            plot_simulations(param_simulations[param], param, parameters[param], SymmetryType,
+                             lambda0, path=path, save=save_fig)
+
+            if anim:
+                animation(param_simulations[param], param, parameters[param],
+                          SymmetryType, lambda0, path=path)
+
+            # Save simulation to future reference.
+            if save:
+                with open(f"{path}/{SymmetryType}_Changing{param}_.bin", "wb") as output:
+                    pickle.dump([param_simulations[param], parameters[param]], output)
 
 
 def assign_two_variables(value1, value2, name1, name2,
@@ -428,33 +452,45 @@ def plot_grid(simulations, parameters, param_values,
 
 if __name__ == "__main__":
 
-    # Define parameter ranges.
-    T_values = np.linspace(10, 100, 5)
-    B_values = 10**np.linspace(-4, -1, 5)
-    delta_values = np.linspace(0, 0.05, 5)
+    if False:
 
-    # Default values when constant.
-    T_default = np.linspace(10, 100, 3)
-    B_default = 10**np.linspace(-4, -1, 3)
-    delta_default = np.linspace(0, 0.05, 3)
+        # Define parameter ranges.
+        T_values = np.linspace(10, 100, 5)
+        B_values = 10**np.linspace(-4, -1, 5)
+        delta_values = np.linspace(0, 0.05, 5)
 
-    # Run 2D surveys with three different default values
-    for i in range(3):
-        print(i, 1)
-        # Simulation without Q branch.
-        two_variable_survey(path=f'Parameter_survey/2D/QFalse/default{i+1}', B_values=B_values,
-                            T_values=T_values, delta_values=delta_values, Q_Branch=False,
-                            B_default=B_default[i], T_default=T_default[i],
-                            delta_default=delta_default[i], load=True)
-        print(i, 2)
-        # Simulation with Q branch
-        two_variable_survey(path=f'Parameter_survey/2D/QTrue/default{i+1}', B_values=B_values,
-                            T_values=T_values, delta_values=delta_values, Q_Branch=True,
-                            B_default=B_default[i], T_default=T_default[i],
-                            delta_default=delta_default[i], load=True)
-        print(i, 3)
-        # Simultion with scaled Q branch
-        two_variable_survey(path=f'Parameter_survey/2D/QScale/default{i+1}', B_values=B_values,
-                            T_values=T_values, delta_values=delta_values, Q_Branch=True,
-                            B_default=B_default[i], T_default=T_default[i],
-                            delta_default=delta_default[i], load=True, Q_scale=0.1)
+        # Default values when constant.
+        T_default = np.linspace(10, 100, 3)
+        B_default = 10**np.linspace(-4, -1, 3)
+        delta_default = np.linspace(0, 0.05, 3)
+
+        # Run 2D surveys with three different default values
+        for i in range(3):
+            print(i, 1)
+            # Simulation without Q branch.
+            two_variable_survey(path=f'Parameter_survey/2D/QFalse/default{i+1}', B_values=B_values,
+                                T_values=T_values, delta_values=delta_values, Q_Branch=False,
+                                B_default=B_default[i], T_default=T_default[i],
+                                delta_default=delta_default[i], load=True)
+            print(i, 2)
+            # Simulation with Q branch
+            two_variable_survey(path=f'Parameter_survey/2D/QTrue/default{i+1}', B_values=B_values,
+                                T_values=T_values, delta_values=delta_values, Q_Branch=True,
+                                B_default=B_default[i], T_default=T_default[i],
+                                delta_default=delta_default[i], load=True)
+            print(i, 3)
+            # Simultion with scaled Q branch
+            two_variable_survey(path=f'Parameter_survey/2D/QScale/default{i+1}', B_values=B_values,
+                                T_values=T_values, delta_values=delta_values, Q_Branch=True,
+                                B_default=B_default[i], T_default=T_default[i],
+                                delta_default=delta_default[i], load=True, Q_scale=0.1)
+
+    if True:
+
+        # Define parameter ranges.
+        A_values = [1, 0.8, 0.4, 0.2, 0.08, 0.05]
+        one_variable_survey(path='Parameter_survey', A_values=A_values)
+
+        # Define parameter ranges.
+        C_values = [0.01, 0.008, 0.004, 0.002, 0.0008]
+        one_variable_survey(path='Parameter_survey', A_values=A_values)
