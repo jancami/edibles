0cb6ef665479390dd1bfcabf89fad702cf55b765
Haoyu Fan <haoyufan@HaoyudeMacBook-Pro.local>
2021-10-28 12:43:37 -0400

    initialize

---
+++ edibles/utils/SightLineModel.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas
+import os
+
+class Species():
+    def __init__(self, name):
+        self.name = name
+
+
+if __name__ == "__main__":
+    print("hello sight line!")
