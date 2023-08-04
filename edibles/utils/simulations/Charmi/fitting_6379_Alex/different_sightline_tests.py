4177b9640c826128b46c827e0ef9ed8f1ba0fab4
alexr <aromanec@uwo.ca>
2023-08-04 16:48:49 -0400

    Comparing model to different sightlines

--- edibles/utils/simulations/Charmi/fitting_6379_Alex/different_sightline_tests.py
+++ edibles/utils/simulations/Charmi/fitting_6379_Alex/different_sightline_tests.py
@@ -5,3 +5,62 @@ Created on Fri Aug  4 12:24:43 2023
 @author: alexr
 """
 
+#%% Parameters
+
+import functions as fn
+import numpy as np
+import matplotlib.pyplot as plt
+
+sightlines = ['24398','144470','147165','147683','149757','166937',
+              '170740','184915','185418','203532']
+
+
+# sightline = sightlines[9]
+sightline = '185859'
+Jmax = 600
+
+#These parameters are given to 3sf however for their uncertainties they may be meant to be given to less
+
+#Parallel best fit parameters:
+B = 0.00129
+delta_B = 0.594
+zeta = -0.969
+T = 49.7
+sigma = 0.198
+origin = -0.0793
+
+# #Perpendicular best fit parameters:
+# B = 0.00179
+# delta_B = 0.717
+# zeta = -1.10
+# T = 28.6
+# sigma = 0.206
+# origin = -0.157
+
+#%% Getting data
+
+#Observational data:
+Obs_data, xs, y_data, std = fn.obs_curve_to_fit(sightline)
+
+#Computing model:
+combinations = fn.allowed_parallel_transitions(Jmax)
+x_mod, y_mod = fn.get_rotational_spectrum(B, delta_B, zeta, T, sigma, origin, combinations, transition = 'parallel', bell = False)
+y_mod_interp = np.interp(xs, x_mod, y_mod)
+
+#%% Chi squared value
+
+chsq = fn.chi_sq(y_mod_interp, y_data, std)
+print(chsq)
+#%% Plotting
+
+fig, ax = plt.subplots()
+ax.plot(xs, y_data, label = 'HD{}'.format(sightline)) # Observational data plot
+ax.plot(xs, y_mod_interp, label = 'T = {}K'.format(str(T))) # Model data plot
+ax.set_title('Chi Squared = {}'.format(chsq))
+ax.xaxis.set_major_locator(plt.MultipleLocator(1))
+ax.xaxis.set_minor_locator(plt.MultipleLocator(0.5))
+ax.set_xlabel('Wavenumber / $cm^{-1}$')
+ax.set_ylabel('Flux')
+ax.legend()
+
+plt.show()
