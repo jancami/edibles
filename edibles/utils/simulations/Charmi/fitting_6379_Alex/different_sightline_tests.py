88fd452df6bf313e1f56e146ac65bf24b41e8ccb
alexr <aromanec@uwo.ca>
2023-08-07 17:05:02 -0400

    Comparison of model to other sightlines

--- edibles/utils/simulations/Charmi/fitting_6379_Alex/different_sightline_tests.py
+++ edibles/utils/simulations/Charmi/fitting_6379_Alex/different_sightline_tests.py
@@ -12,7 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sightlines = ['24398','144470','147165','147683','149757','166937',
-              '170740','184915','185418','203532']
+              '170740','184915','185418','203532']#,'185859']
 
 
 # sightline = sightlines[9]
@@ -22,12 +22,12 @@ Jmax = 600
 #These parameters are given to 3sf however for their uncertainties they may be meant to be given to less
 
 #Parallel best fit parameters:
-B = 0.00129
-delta_B = 0.594
-zeta = -0.969
-T = 49.7
-sigma = 0.198
-origin = -0.0793
+B = 0.00179
+delta_B = 0.717
+zeta = -1.10
+T = 28.6
+sigma = 0.206
+origin = 0.157
 
 # #Perpendicular best fit parameters:
 # B = 0.00179
@@ -43,8 +43,8 @@ origin = -0.0793
 Obs_data, xs, y_data, std = fn.obs_curve_to_fit(sightline)
 
 #Computing model:
-combinations = fn.allowed_parallel_transitions(Jmax)
-x_mod, y_mod = fn.get_rotational_spectrum(B, delta_B, zeta, T, sigma, origin, combinations, transition = 'parallel', bell = False)
+combinations = fn.allowed_perperndicular_transitions(Jmax)
+x_mod, y_mod = fn.get_rotational_spectrum(B, delta_B, zeta, T, sigma, origin, combinations, transition = 'perpendicular', bell = False)
 y_mod_interp = np.interp(xs, x_mod, y_mod)
 
 #%% Chi squared value
@@ -53,9 +53,14 @@ chsq = fn.chi_sq(y_mod_interp, y_data, std)
 print(chsq)
 #%% Plotting
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots()#figsize = (15,10))
 ax.plot(xs, y_data, label = 'HD{}'.format(sightline)) # Observational data plot
-ax.plot(xs, y_mod_interp, label = 'T = {}K'.format(str(T))) # Model data plot
+# for star in sightlines: # For plotting multiple stars 
+#     Obs_data, x, y, std = fn.obs_curve_to_fit(star)
+#     ax.plot(x, y, label = 'HD{}'.format(star), linewidth = 1.3)#, linestyle = '-.')
+# Obs_data, x, y, std = fn.obs_curve_to_fit(sightline)
+# ax.plot(x, y, label = 'HD{}'.format(sightline), linewidth = 3)
+# ax.plot(xs, y_mod_interp, label = 'HD185859 Model', linewidth = 4) # Model data plot
 ax.set_title('Chi Squared = {}'.format(chsq))
 ax.xaxis.set_major_locator(plt.MultipleLocator(1))
 ax.xaxis.set_minor_locator(plt.MultipleLocator(0.5))
