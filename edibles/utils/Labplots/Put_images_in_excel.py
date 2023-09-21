85ce2abd7625b7b161795bf04fab9f7f1f6dcc86
Charmi Bhatt <charmi.bhatt2051@gmail.gom>
2023-09-21 18:31:04 -0400

    Python script to put images in an excel sheet (by row)

---
+++ edibles/utils/Labplots/Put_images_in_excel.py
@@ -0,0 +1,37 @@
+import os
+from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.drawing.image import Image
+from PIL import Image as PILImage
+
+# Create a new workbook and select the active worksheet
+workbook = Workbook()
+worksheet = workbook.active
+
+# Directory where your images are stored
+image_dir = '/Users/charmibhatt/Desktop/Edibles_All/Edibles_Scripts/edibles/utils/Labplots/Haoyus_sightlines_plots'
+
+
+# Loop through the files and add them to the Excel file
+files = os.listdir(image_dir)
+
+# Loop through the files and add them to the Excel file
+for index, file in enumerate(files):
+    if file.endswith(('jpeg', 'jpg', 'png')):  # Add other file types if needed
+        img = Image(os.path.join(image_dir, file))
+        
+        # Adjust the size of the image if necessary (optional)
+        pil_img = PILImage.open(os.path.join(image_dir, file))
+        img.width, img.height = pil_img.size
+
+        # Add the image to the worksheet
+        worksheet.add_image(img, f'A{index + 1}')
+
+        # Insert filename in the next column
+        worksheet[f'B{index + 1}'] = file
+
+        # Set the row height to match the image height (optional)
+        worksheet.row_dimensions[index + 1].height = img.height
+
+# Save the workbook
+workbook.save('labplots_haoyus_sightlines_poly1d_normalization.xlsx')
