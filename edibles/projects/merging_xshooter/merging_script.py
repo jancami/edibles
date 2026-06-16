60b713261035511ba9429292c63767f385635048
Alexander Ebenbichler <alexander@ebenbichler.net>
2026-06-16 11:40:39 -0400

    outline of the idea

---
+++ edibles/projects/merging_xshooter/merging_script.py
@@ -0,0 +1,26 @@
+"""
+Merging EDIBLES spectra with help of XSHOOTER
+"""
+from PyAstronomy import pyasl
+
+# stitch XSHOOTER spectra (later)
+
+# load XSHOOTER spectrum
+
+# for each EDIBLES order
+
+# apply instrumental broadening of XSHOOTER to uves
+
+# pyasl.instrBroadGaussFast
+
+# crop XSHOOTER to EDIBLES order
+
+# resample XSHOOTER to EDIBLES or vice versa
+
+# calculate flux ratio XSHOOTER/EDIBLES or vv
+
+# fit smooth curve ro ratio
+
+# normalize EDIBLES with curve
+
+# Coadd all EDIBLES spectra
